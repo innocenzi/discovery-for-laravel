a3d9a4d78d0d4c89c165516b3be6ad5e128d2226
Enzo Innocenzi <enzo@innocenzi.dev>
2026-04-08 11:47:42 +0200

    refactor: remove built-in hybridly discovery

--- src/DiscoveryServiceProvider.php
+++ src/DiscoveryServiceProvider.php
@@ -4,15 +4,9 @@ declare(strict_types=1);
 
 namespace Discovery;
 
-use Discovery\Hybridly\DefaultHybridComponentNameResolver;
-use Discovery\Hybridly\HybridComponentNameResolver;
-use Discovery\Hybridly\StaticComponentsResolver;
-use Hybridly\Architecture\ComponentsResolver;
-use Hybridly\Hybridly;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Symfony\Component\Cache\Adapter\PhpFilesAdapter;
-use Tempest\Discovery\Composer;
 use Tempest\Discovery\DiscoveryCache;
 use Tempest\Discovery\DiscoveryCacheStrategy;
 use Tempest\Discovery\DiscoveryConfig;
@@ -33,26 +27,7 @@ final class DiscoveryServiceProvider extends ServiceProvider
             key: 'discovery',
         );
 
-        $this->registerHybridly();
         $this->registerDiscovery();
-    }
-
-    private function registerHybridly(): void
-    {
-        if (! class_exists(Hybridly::class)) {
-            return;
-        }
-
-        $this->app->singleton(ComponentsResolver::class, static fn () => new StaticComponentsResolver());
-
-        $this->app->singleton(HybridComponentNameResolver::class, static function () {
-            $root = config('discovery.autoload_path');
-
-            return new DefaultHybridComponentNameResolver(
-                composer: new Composer($root)->load(),
-                root_path: $root,
-            );
-        });
     }
 
     private function registerDiscovery(): void
